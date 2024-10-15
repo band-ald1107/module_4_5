2e59742517cd190c52674a6fa07235f0837a5b53
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-10-15 08:02:14 +0800

    Add files via upload

---
+++ module_4_5.py
@@ -0,0 +1,32 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(House, cls).__new__(cls)
+        if args:
+            cls.houses_history.append(args[0])
+
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+h3 = House('ЖК Матрёшки', 20)
+
+
+print(House.houses_history)
+
+
+del h1
+del h2
+
+
+print(House.houses_history)
